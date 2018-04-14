5dfcca11354c0376585f6f3ed1ba3b36c21668f2
ConstantineGochev <constantingochev1993@gmail.com>
2018-04-14 13:08:41 +0300

    ignore

---
+++ config/keys.js~
@@ -0,0 +1,6 @@
+module.exports = {
+
+      googleClientID: '645080786682-t5rfj2meokvvfuqhkpic8htk5qljv0b9.apps.googleusercontent.com',
+      googleClientSecret: ' Wpbetugbi8Prwc7AWLrUYj-g'
+
+       } 
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
+//cliendID === 645080786682-t5rfj2meokvvfuqhkpic8htk5qljv0b9.apps.googleusercontent.com
+//  cliendSecret === Wpbetugbi8Prwc7AWLrUYj-g
 
 const app = express();
 
 
-app.get('/',(req, res) => {
-     res.send('minimal');
-    })
-
+passport.use(GoogleStrategy());
 
 
 const port = process.env.PORT || 3000
