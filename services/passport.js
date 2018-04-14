c7e3c0170f555d2392b38e3f71b0e9d8c5bb2963
ConstantineGochev <constantingochev1993@gmail.com>
2018-04-14 14:38:34 +0300

    passport

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users')
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -10,5 +13,21 @@ passport.use(new GoogleStrategy({
     console.log('access token == %s',accessToken);
     console.log('refresh token == %s',refreshToken);
     console.log('profle == %s',profile);
+    User.findOne({
+        googleId: profile.id
+    }).then((existingUser) => {
+        if(existingUser){
+            done(null, existingUser)
+            console.log('tehe is already user')
+        }else{
+            new User({
+                googleId: profile.id
+            })
+            .save()
+            .then((user) => {
+                done(null, user)
+            })
+        }
+    })
     
 }));
